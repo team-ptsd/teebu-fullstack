94cf3de0871fdfe3ada112bb5218f26da8703469
kkukileon305 <kkukileon305@gmail.com>
2023-05-10 22:07:17 +0900

    feat: add theme state and skeleton ui when not window

--- src/components/client/buttons/ThemeSwitchBtn.tsx
+++ src/components/client/buttons/ThemeSwitchBtn.tsx
@@ -1,17 +1,24 @@
 'use client';
 
-import Cookies from 'js-cookie';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+import useClient from '@/hooks/useClient';
 
 const ThemeSwitchBtn = () => {
+  const isClient = useClient();
+  const [theme, setTheme] = useState(Cookies.get('theme') === 'dark' ? 'dark' : 'light');
   const router = useRouter();
 
   const onClick = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
     Cookies.set('theme', Cookies.get('theme') === 'dark' ? 'light' : 'dark');
     router.refresh();
   };
 
-  return <button onClick={onClick}>ThemeSwitchBtn</button>;
+  if (!isClient) return <div className='w-9 h-6 bg-gray-200 dark:bg-slate-400 rounded' />;
+
+  return <button onClick={onClick}>{theme}</button>;
 };
 
 export default ThemeSwitchBtn;
