c885cb8041eef1df689f76f13ce8e51636537227
kkukileon305 <kkukileon305@gmail.com>
2023-05-10 22:53:10 +0900

    add email input

--- src/components/client/forms/auth/SignInForm.tsx
+++ src/components/client/forms/auth/SignInForm.tsx
@@ -16,7 +16,17 @@ const SignInForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <h3 className={'font-bold text-2xl text-center'}>로그인</h3>
+      <h3 className={'font-bold text-2xl text-center mb-8'}>로그인</h3>
+
+      <div className={'flex flex-col gap-2'}>
+        <label htmlFor={'email'}>이메일</label>
+        <input
+          id={'email'}
+          type={'email'}
+          {...register('email', { required: true })}
+          className={'border border-gray-300 dark:border-gray-700 rounded-md p-2'}
+        />
+      </div>
     </form>
   );
 };
