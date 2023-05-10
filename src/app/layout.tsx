ac954b78b2dbb41f0a4a56021e357fa37b3fcfbb
kkukileon305 <kkukileon305@gmail.com>
2023-05-10 21:40:24 +0900

    feat: add Header components and background style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import './globals.css';
+
 import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import { Noto_Serif_KR } from 'next/font/google';
 
 import SupabaseProvider from '@/app/supabase-provider';
-
-import './globals.css';
+import Header from '@/components/server/panels/Header';
 
 const baseFont = Noto_Serif_KR({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -17,7 +18,10 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang='kr'>
       <body className={baseFont.className}>
-        <SupabaseProvider>{children}</SupabaseProvider>
+        <SupabaseProvider>
+          <Header />
+          {children}
+        </SupabaseProvider>
       </body>
     </html>
   );
---
+++ src/components/server/panels/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className='bg-white dark:bg-slate-600 shadow'>Header</header>;
+};
+
+export default Header;
